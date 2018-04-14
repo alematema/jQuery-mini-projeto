90f4c4abbebbbb5dde60f00c6af96616c36bc824
Alexandre Siqueira <alematema@gmail.com>
2018-04-14 18:56:12 -0300

    add botao remover no placar

--- js/main.js
+++ js/main.js
@@ -64,21 +64,50 @@ function reiniciarJogo() {
 function inserePlacar() {
 
 	var corpoTabela = $('.placar').find('tbody');
-//	var corpoTabela = document.querySelector('tbody');
-	var usuario = 'Alex';
+	//	var corpoTabela = document.querySelector('tbody');
+	var usuario = 'alex_andré';
 	var numPalavras = $('#contador-palavras').text();
-	
-	var linha = '<tr>'+
-									'<td>'+usuario+'</td>'+
-									'<td>'+numPalavras+'</td>'+
-							'</tr>';
-	
+
+	var linha = novaLinha(usuario, numPalavras);
+
 	corpoTabela.prepend(linha);
-	
+
+}
+
+function novaLinha(usuario, numPalavras) {
+
+	var linha = $('<tr>');
+
+	var colunaUsuario = $('<td>').text(usuario);
+	var colunaPalavras = $('<td>').text(numPalavras);
+	var colunaRemover = $('<td>');
+
+	var link = $('<a>').addClass('botao-remover').attr('href', '#');
+	var icone = $('<i>').addClass('small').addClass('material-icons').text('delete_forever');
+
+	link.append(icone);
+	colunaRemover.append(link);
+
+	link.click(removeLinha);
+
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha;
+
+}
+
+
+function removeLinha(event) {
+
+	event.preventDefault();
+	$(this).parent().parent().remove();
+
 }
 
 function inicializarCronometro() {
-	
+
 	campo.one('focus', function () {
 
 
